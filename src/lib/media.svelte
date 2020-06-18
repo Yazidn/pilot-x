2362ad89fe0a52472b1ec12d286bfc4def2d256f
Yazid <secho771@gmail.com>
2020-06-18 12:52:23 +0100

    better usability

--- src/lib/media.svelte
+++ src/lib/media.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { scale } from 'svelte/transition';
+  import { scale } from "svelte/transition";
 
   import { MediaStore, Preferences } from "../stores.js";
   let displayEditor = $Preferences.displayEditor;
@@ -37,46 +37,73 @@
 </script>
 
 <main
-  transition:scale={{duration: 300}}
+  transition:scale={{ duration: 300 }}
   class="h-full shadow-2xl rounded-md bg-cover transform hover:-translate-y-2
-  transition-transform duration-300"
+  transition-transform duration-300 {watchLater ? 'opacity-50' : ''}"
   style="background-image: url({poster})">
-  <section
-    class="flex flex-col justify-between text-white gradient
-    h-full p-4">
+  <section class="flex flex-col justify-between text-white gradient h-full p-4">
     <div>
 
-      {#if watchLater}
-        <p>Watch later</p>
-      {/if}
-
       {#if displayType}
-        <p>{type}</p>
+        <p>
+          <i class="fas fa-film" />
+          {type}
+        </p>
       {/if}
 
-      <h1 class="cursor-pointer" on:click={() => (showDetails = true)}>
-        {title}
-      </h1>
+      {#if watchLater}
+        <h1
+          class="cursor-pointer hover:text-gray-300"
+          on:click={() => (showDetails = true)}>
+          <i class="fas fa-clock" />
+          {title}
+        </h1>
+      {:else}
+        <h1
+          class="cursor-pointer hover:text-gray-300"
+          on:click={() => (showDetails = true)}>
+          {title}
+        </h1>
+      {/if}
 
-      {#if type === 'Series'}
+      {#if type === 'Series' && !watchLater}
         <h3>Season {season}</h3>
 
         {#if episodeEditMode}
-          <input type="number" bind:value={episode} on:blur={updateEpisode} />
+          <input
+            class="pt-8 text-4xl w-full bg-transparent text-center outline-none"
+            type="text"
+            bind:value={episode}
+            on:blur={updateEpisode}
+            on:keydown={e => (e.which === 13 || e.which === 27 ? updateEpisode() : '')}
+            autofocus />
         {:else}
-          <h2 on:click={() => (episodeEditMode = true)}>{episode}</h2>
+          <h2
+            class="pt-8 text-4xl text-center cursor-pointer hover:text-gray-300"
+            on:click={() => (episodeEditMode = true)}>
+            {episode}
+          </h2>
         {/if}
       {/if}
 
-      {#if timestampEditMode}
-        <input
-          type="text"
-          bind:value={timestamp}
-          step="1"
-          on:blur={updateTimestamp} />
-      {:else}
-        <h4 on:click={() => (timestampEditMode = true)}>{timestamp}</h4>
+      {#if !watchLater}
+        {#if timestampEditMode}
+          <input
+            class="w-full text-center bg-transparent outline-none"
+            type="text"
+            bind:value={timestamp}
+            on:keydown={e => (e.which === 13 || e.which === 27 ? updateTimestamp() : '')}
+            on:blur={updateTimestamp}
+            autofocus />
+        {:else}
+          <h4
+            class="text-center cursor-pointer hover:text-gray-300"
+            on:click={() => (timestampEditMode = true)}>
+            {timestamp}
+          </h4>
+        {/if}
       {/if}
+
     </div>
 
     {#if displayEditor}
@@ -96,5 +123,12 @@
     on:hideDetails={() => (showDetails = false)} />
 {/if}
 {#if showDetails}
-	<Details {id} {season} {episode} {title} {link} {misc} on:hideDetails={() => showDetails = false}/>
+  <Details
+    {id}
+    {season}
+    {episode}
+    {title}
+    {link}
+    {misc}
+    on:hideDetails={() => (showDetails = false)} />
 {/if}
