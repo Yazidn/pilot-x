70f92d7fed397fc77f50fbb960b7694d9a1d7258
Yazid <secho771@gmail.com>
2020-07-08 16:13:00 +0100

    added logo to the app and it acts as a home button
    also moved to the middle

--- src/lib/navigation.svelte
+++ src/lib/navigation.svelte
@@ -2,28 +2,28 @@
   import { current } from "../router.js";
 </script>
 
-<main class="fixed inset-x-0 bottom-0 flex justify-center p-8 z-10">
+<main class="fixed inset-x-0 bottom-0 flex justify-center p-10 z-10">
   <div class="w-full md:w-3/4 lg:w-2/4 flex justify-evenly">
+
     <button
       class="bg-white h-12 w-12 bg-white rounded-full shadow-lg transform
-      hover:-translate-y-2 transition-transform hover:bg-black hover:text-white
-      transition-colors duration-300"
-      on:click={() => current.set('home')}
-      title="Home">
-      <i class="fas fa-th" />
-    </button>
-    <button
-      class="bg-white h-12 w-12 bg-white rounded-full shadow-lg transform
-      hover:-translate-y-2 transition-transform hover:bg-black hover:text-white
-      transition-colors duration-300"
+      hover:-translate-y-2 transition-all hover:bg-black hover:text-white
+      duration-300"
       on:click={() => current.set('add')}
       title="Add">
       <i class="fas fa-plus" />
     </button>
+
+    <img
+      class="h-12 cursor-pointer"
+      src="./logo.png"
+      alt="logo"
+      on:click={() => current.set('home')} />
+
     <button
       class="bg-white h-12 w-12 bg-white rounded-full shadow-lg transform
-      hover:-translate-y-2 transition-transform hover:bg-black hover:text-white
-      transition-colors duration-300"
+      hover:-translate-y-2 transition-all hover:bg-black hover:text-white
+      duration-300"
       on:click={() => current.set('preferences')}
       title="Preferences">
       <i class="fas fa-sliders-h" />
